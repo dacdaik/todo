634256736f2430c0bf6db53890a3fb57207a0cfa
unknown <caobadacck6@gmail.com>
2022-12-24 01:30:31 +0800

    todoapp

--- src/pages/Todonew/TodoHook.js
+++ src/pages/Todonew/TodoHook.js
@@ -1,18 +1,36 @@
-import React, { useRef, useState } from "react";
+import axios from "axios";
+import React, { useEffect , useState } from "react";
 import "./TodoApp.css";
 import TodoFooter from "./TodoFooter";
 import TodoList from "./TodoList";
 
 const TodoHook = () => {
-    const [todo, setTodo] = useState(
-        [{ id: 1, content: 'ăn sáng', isActive: false, isComplete: false },
-        { id: 2, content: 'ăn trưa', isActive: false, isComplete: false },
-        { id: 3, content: 'ăn tối', isActive: false, isComplete: true },]
-    );
+    const [todo, setTodo] = useState([]);
+    const fetchListTodo = async () => {
+      const {data} = await axios.get("https://63a44e2a821953d4f2b0637d.mockapi.io/app");
+      setTodo(data);
+    }
+    useEffect(()=>{
+      fetchListTodo()
+    },[])
 
+    const changInput = event => {
+      const addName=(
+        {content: event.target.value, isComplete: 'false', isActive: 'false'});
+        // setTodo(addName);
+      // console.log(event.target.value);
+      console.log(addName);
+    }
+  
+    const handleSubmit = event => {
+      event.preventDefault();
+      console.log(todo);
+  
+      const user = {};
+    }
   return (
     <section className="todoapp">
-      <div data-reactid=".0">
+      <div data-reactid=".0" >
         <header className="header" data-reactid=".0.0">
           <h1 data-reactid=".0.0.0">todos</h1>
           <input
@@ -20,11 +38,16 @@ const TodoHook = () => {
             placeholder="What needs to be done?"
             defaultValue=""
             data-reactid=".0.0.1"
+            onChange = {changInput}
+            onKeyPress = {(e) => {
+              if(e.key === "Enter") {
+                  console.log("Cao Ba Dac");;
+              }
+          }}
           />
         </header>
         <TodoList todo = {todo} />
         <TodoFooter />
-        
       </div>
     </section>
   );
--- src/pages/Todonew/TodoList.js
+++ src/pages/Todonew/TodoList.js
@@ -1,22 +1,50 @@
 import React from 'react'
 
-export default function TodoList(todo) {
-    console.log(typeof todo, todo.length);
-    // todo.filter((item)=>{
-    //     console.log(item);
-    // })
+const TodoList = (props) =>{
   return (
     <section className="main" data-reactid=".0.1">
-        <input
-          id="toggle-all"
-          className="toggle-all"
-          type="checkbox"
-           data-reactid=".0.1.0"
-        />
-        <label htmlFor="toggle-all" data-reactid=".0.1.1" />
-        <ul className="todo-list" data-reactid=".0.1.2">
-            
-        </ul>
-    </section>
+            <input
+              id="toggle-all"
+              className="toggle-all"
+              type="checkbox"
+              data-reactid=".0.1.0"
+            />
+            <label htmlFor="toggle-all" data-reactid=".0.1.1" />
+            <ul className="todo-list" data-reactid=".0.1.2">
+              {props.todo.map((item, index)=>{
+                return (
+                  <li
+                    className=""
+                    data-reactid=".0.1.2.$bb632cfd-6960-41f0-a68e-5387c4a20654"
+                    key = {index}
+                  >
+                    <div
+                      className="view"
+                      data-reactid=".0.1.2.$bb632cfd-6960-41f0-a68e-5387c4a20654.0"
+                    >
+                    <input
+                      className="toggle"
+                      type="checkbox"
+                      data-reactid=".0.1.2.$bb632cfd-6960-41f0-a68e-5387c4a20654.0.0"
+                    />
+                      <label data-reactid=".0.1.2.$bb632cfd-6960-41f0-a68e-5387c4a20654.0.1">
+                        {item.content}
+                      </label>
+                      <button
+                        className="destroy"
+                        data-reactid=".0.1.2.$bb632cfd-6960-41f0-a68e-5387c4a20654.0.2"
+                      />
+                    </div>
+                    <input
+                      className="edit"
+                      defaultValue="ăn sáng"
+                      data-reactid=".0.1.2.$bb632cfd-6960-41f0-a68e-5387c4a20654.1"
+                    />
+                  </li>
+                 )
+              })}
+            </ul>
+          </section>
   )
 }
+export default TodoList;
